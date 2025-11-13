6aa4c57e265bdeeb4656b68b3659d303a809a946
Juliette De Meo <gigialiasjuliette@gmail.com>
2025-11-13 09:33:37 +0100

    made logo

--- client/src/components/Logo/Logo.tsx
+++ client/src/components/Logo/Logo.tsx
@@ -1,11 +1,22 @@
 import "./logo.css";
+
+interface meridianStyle extends React.CSSProperties {
+  "--start": string;
+  "--end": string;
+  "--rotateY": string;
+}
+
 export default function Logo() {
-  const count = 10;
+  const count = 8;
   const step = 180 / count;
 
   return (
     <div className="logo">
-      <svg viewBox="-500 -500 1000 1000">
+      <svg
+        viewBox="-500 -500 1000 1000"
+        aria-label="Rotating meridians sphere animation"
+      >
+        <title>Rotating Meridians</title>
         <circle r="450" />
 
         {Array.from({ length: count }, (_, i) => {
@@ -14,14 +25,16 @@ export default function Logo() {
 
           return (
             <circle
-              key={i}
+              key={`meridian-${start}-${end}`}
               className="meridian"
               r="450"
-              style={{
-                "--start": `${start}deg`,
-                "--end": `${end}deg`,
-                "--rotateY": `${start}deg`,
-              }}
+              style={
+                {
+                  "--start": `${start}deg`,
+                  "--end": `${end}deg`,
+                  "--rotateY": `${start}deg`,
+                } as meridianStyle
+              }
             />
           );
         })}
